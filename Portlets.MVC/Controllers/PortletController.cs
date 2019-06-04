be9780487ec324f260a191213d6e51508529fdfe
Jacobs <mathew.jacobs@sinclair.edu>
2019-06-04 14:56:47 -0400

    major improvements to booklist and class schedule

--- Portlets.MVC/Controllers/PortletController.cs
+++ Portlets.MVC/Controllers/PortletController.cs
@@ -99,12 +99,13 @@ namespace Portlets.MVC.Controllers
                 }
             }
 
-            string bearerToken = admin.Login();
+            var books = new List<Book>();
 
-            var books = new BookList()
+            var schedule = GetClassSchedule(Id);
+            foreach (var row in schedule.rows)
             {
-                Books = GetBookList(Id, bearerToken)
-            };
+                books.AddRange(row.booklist);
+            }
 
             return View(books);
         }
@@ -124,14 +125,9 @@ namespace Portlets.MVC.Controllers
                 }
             }
 
-            string bearerToken = admin.Login();
+            var schedule = GetClassSchedule(Id);
 
-            var classSchedule = new ClassSchedule()
-            {
-                CourseSections = GetClassSchedule(Id, bearerToken)
-            };
-
-            return View(classSchedule);
+            return View(schedule);
         }
 
         // Methods
@@ -185,266 +181,15 @@ namespace Portlets.MVC.Controllers
             SU,
             FA
         }
-        private AcademicData GetCurrentClasses(string Id, string bearerToken)
+        private Schedule GetClassSchedule(string Id)
         {
-
-            string term = GetCurrentTerm(bearerToken);
-
-            RequestHeader[] headers =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "Content-Type", Value = "application/json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken }
-            };
-
-            var body = new { BestFit = false, Filter = false, IncludeStudentSections = true, StudentIds = new string[] { Id }, term };
-
-
-            var response = utility.CreateRequest(Method.POST, "https://api.sinclair.edu/colleagueapi", $"/qapi/academic-history", headers, body);
+            Id = (long.Parse(Id) * 4567).ToString();
+            var response = utility.CreateRequest(Method.GET, "https://rest.sinclair.edu/api/1/index.cfm", $"/Bulletin/CurrentCourses/{Id}");
             dynamic jsonContent = JValue.Parse(response.Content);
-            List<AcademicData> obj = jsonContent.ToObject<List<AcademicData>>();
-            return obj[0];
-        }
-        private string GetCurrentTerm(string bearerToken)
-        {
-            var date = DateTime.Now;
-            var year = date.Year.ToString().Substring(2);
-            List<Term> terms = new List<Term>();
+            Schedule sections = jsonContent.ToObject<Schedule>();
 
-            RequestHeader[] headers =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "Content-Type", Value = "application/json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken }
-            };
-            var winterRe = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/terms?id={year}/WI", headers);
-            if (winterRe.StatusCode == HttpStatusCode.OK)
-            {
-                dynamic jsonWinter = JValue.Parse(winterRe.Content);
-                Term winterTerm = jsonWinter.ToObject<Term>();
-                terms.Add(winterTerm);
-            }
-            var springRe = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/terms?id={year}/SP", headers);
-            if (springRe.StatusCode == HttpStatusCode.OK)
-            {
-                dynamic jsonSpring = JValue.Parse(springRe.Content);
-                Term springTerm = jsonSpring.ToObject<Term>();
-                terms.Add(springTerm);
-            }
-            var summerRe = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/terms?id={year}/SU", headers);
-            if (summerRe.StatusCode == HttpStatusCode.OK)
-            {
-                dynamic jsonSummer = JValue.Parse(summerRe.Content);
-                Term summerTerm = jsonSummer.ToObject<Term>();
-                terms.Add(summerTerm);
-            }
-            var fallRe = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/terms?id={year}/FA", headers);
-            if (fallRe.StatusCode == HttpStatusCode.OK)
-            {
-                dynamic jsonFall = JValue.Parse(fallRe.Content);
-                Term fallTerm = jsonFall.ToObject<Term>();
-                terms.Add(fallTerm);
-            }
-            foreach (var term in terms)
-            {
-                if (term.StartDate < date && term.EndDate > date)
-                {
-                    return term.Code;
-                }
-            }
-            return null;
-        }
-        private List<Book> GetBookList(string Id, string bearerToken)
-        {
-            List<Book> books = new List<Book>();
-            AcademicData data = GetCurrentClasses(Id, bearerToken);
-            List<CourseCatalog> catalogs = new List<CourseCatalog>();
-            string courseIds = "";
-
-            if (data.AcademicTerms.Count == 0)
-            {
-                return null;
-            }
-
-            foreach (var course in data.AcademicTerms[0].AcademicCredits)
-            {
-                courseIds = courseIds + course.CourseId + ",";
-
-                var keyword = course.CourseName;
-                var term = data.AcademicTerms[0].TermId;
-                var subjectCode = course.CourseName.Split('-')[0];
-                var subjectNum = course.CourseName.Split('-')[1];
-
-                var response = utility.CreateRequest(Method.GET, "https://rest.sinclair.edu/api/1/index.cfm", $"/Bulletin/Sections/{subjectCode}/{subjectNum}/{term.Replace("/", "")}?keyword={course.CourseName}&courseList=any&term={term.Replace("/", "")}&subjectCode={subjectCode}&building=any&courseFormat=all&termFormat=all&creditHoursMin=0&creditHoursMax=15&timeChoice=segments&segOptions=any");
-                dynamic jsonContent = JValue.Parse(response.Content);
-                CourseCatalog obj = jsonContent.ToObject<CourseCatalog>();
-                catalogs.Add(obj);
-            }
-
-            RequestHeader[] headers =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken }
-            };
-
-            var response2 = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/courses/sections?courseIds={courseIds}", headers);
-            dynamic jsonContent2 = JValue.Parse(response2.Content);
-            List<CourseSection> sections = jsonContent2.ToObject<List<CourseSection>>();
-
-            foreach (var courseCatalog in catalogs)
-            {
-                foreach (var row in courseCatalog.rows)
-                {
-                    foreach (var section in sections)
-                    {
-                        if (section.ActiveStudentIds.Contains(Id) && row.CourseKey.ToString() == section.Id)
-                        {
-                            foreach (var book in row.booklist)
-                            {
-                                books.Add(book);
-                            }
-                        }
-                    }
-                }
-            }
-            List<Book> results = books.Distinct().ToList();
-            return results;
+            return sections;
         }
 
-        private List<CourseInfo> GetClassSchedule(string Id, string bearerToken)
-        {
-            List<CourseInfo> info = new List<CourseInfo>();
-            List<CourseSection> courses = new List<CourseSection>();
-
-            List<CourseCatalog> catalogs = new List<CourseCatalog>();
-            List<string> facultyIds = new List<string>();
-            AcademicData data = GetCurrentClasses(Id, bearerToken);
-            List<Location> loc = GetLocations(bearerToken);
-            string courseIds = "";
-            foreach (var course in data.AcademicTerms[0].AcademicCredits)
-            {
-                courseIds = courseIds + course.CourseId + ",";
-
-                var keyword = course.CourseName;
-                var term = data.AcademicTerms[0].TermId;
-                var subjectCode = course.CourseName.Split('-')[0];
-                var subjectNum = course.CourseName.Split('-')[1];
-
-                var r = utility.CreateRequest(Method.GET, "https://rest.sinclair.edu/api/1/index.cfm", $"/Bulletin/Sections/{subjectCode}/{subjectNum}/{term.Replace("/", "")}?keyword={course.CourseName}&courseList=any&term={term.Replace("/", "")}&subjectCode={subjectCode}&building=any&courseFormat=all&termFormat=all&creditHoursMin=0&creditHoursMax=15&timeChoice=segments&segOptions=any");
-                dynamic jC = JValue.Parse(r.Content);
-                CourseCatalog obj = jC.ToObject<CourseCatalog>();
-                catalogs.Add(obj);
-            }
-
-            RequestHeader[] headers =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken }
-            };
-
-            var response = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", $"/courses/sections?courseIds={courseIds}", headers);
-            dynamic jsonContent = JValue.Parse(response.Content);
-            List<CourseSection> sections = jsonContent.ToObject<List<CourseSection>>();
-
-            foreach (var course in sections)
-            {
-                if (course.ActiveStudentIds.Contains(Id) && !courses.Contains(course))
-                {
-                    courses.Add(course);
-                    foreach (var id in course.FacultyIds)
-                    {
-                        if (!facultyIds.Contains(id.ToString()))
-                        {
-                            facultyIds.Add(id.ToString());
-                        }
-                    }
-
-                }
-            }
-            RequestHeader[] facultyHeaders =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken },
-                new RequestHeader() { Key = "Content-Type", Value = "application/json" }
-            };
-            var body = new { FacultyIds = facultyIds.ToArray() };
-            var facultyRe = utility.CreateRequest(Method.POST, "https://api.sinclair.edu/colleagueapi", "/qapi/faculty", headers, body);
-            dynamic jsonFaculty = JValue.Parse(facultyRe.Content);
-            List<Faculty> faculty = jsonFaculty.ToObject<List<Faculty>>();
-            foreach (var course in courses)
-            {
-                course.Faculties = new List<Faculty>();
-                foreach (var id in course.FacultyIds)
-                {
-                    foreach (var person in faculty)
-                    {
-                        if (person.Id == id.ToString())
-                        {
-                            course.Faculties.Add(person);
-                        }
-                    }
-                }
-            }
-
-            foreach (var courseCatalog in catalogs)
-            {
-                foreach (var row in courseCatalog.rows)
-                {
-                    foreach (var section in sections)
-                    {
-                        if (section.ActiveStudentIds.Contains(Id) && row.CourseKey.ToString() == section.Id)
-                        {
-                            foreach (var location in loc)
-                            {
-                                if ((location.Code == section.Location || section.Location == ""))
-                                {
-                                    CourseInfo temp = new CourseInfo()
-                                    {
-                                        CourseId = section.CourseId,
-                                        Instructors = section.Faculties,
-                                        CourseName = row.CourseCode,
-                                        Loc = location,
-                                        Meetings = section.Meetings,
-                                        Title = row.LongName,
-                                        StartDate = section.StartDate,
-                                        EndDate = section.EndDate,
-                                        Room = row.building,
-                                        Section = row.SectionNo.ToString()
-                                    };
-
-                                    info.Add(temp);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            return info;
-        }
-
-        private List<Location> GetLocations(string bearerToken)
-        {
-            RequestHeader[] headers =
-            {
-                new RequestHeader() { Key = "Accept", Value = "application/vnd.ellucian.v1+json" },
-                new RequestHeader() { Key = "X-CustomCredentials", Value = bearerToken }
-            };
-            var response = utility.CreateRequest(Method.GET, "https://api.sinclair.edu/colleagueapi", "/locations", headers);
-            dynamic jsonResponse = JValue.Parse(response.Content);
-            return jsonResponse.ToObject<List<Location>>();
-        }
-
-        private static bool FindISBN(Book bk, string isbn)
-        {
-            if (bk.isbn == isbn)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
--- Portlets.MVC/Models/Schedule.cs
+++ Portlets.MVC/Models/Schedule.cs
@@ -5,6 +5,13 @@ using System.Web;
 
 namespace Portlets.MVC.Models
 {
+    public class Schedule
+    {
+        public int results { get; set; }
+        public bool success { get; set; }
+        public List<Row> rows { get; set; }
+    }
+
     public class DayModel
     {
         public string location { get; set; }
@@ -13,67 +20,69 @@ namespace Portlets.MVC.Models
         public string endTime { get; set; }
         public object SectionNo { get; set; }
         public string title { get; set; }
-        public int? eventId { get; set; }
+        public int eventId { get; set; }
         public string term { get; set; }
-        public string satLocation { get; set; }
-        public int? calendarId { get; set; }
+        public int calendarId { get; set; }
+    }
+
+    public class Book
+    {
+        public object SectionNo { get; set; }
+        public string priceNew { get; set; }
+        public string author { get; set; }
+        public string Term { get; set; }
+        public string reqRecOpt { get; set; }
+        public string CourseCode { get; set; }
+        public string isbn { get; set; }
+        public string priceUsed { get; set; }
+        public string title { get; set; }
+        public object copyrightYear { get; set; }
+        public object edition { get; set; }
+        public object CourseNumber { get; set; }
+        public string SubjectCode { get; set; }
     }
 
     public class Row
     {
-        public string location { get; set; }
-        public int? sortOrderTwo { get; set; }
         public string startDate { get; set; }
         public string regEndDate { get; set; }
         public string EndTime { get; set; }
         public string flexFlag { get; set; }
         public string Days { get; set; }
-        public int? seatCount { get; set; }
+        public int seatCount { get; set; }
         public string comments { get; set; }
         public string restrictions { get; set; }
         public string courseSysnonum { get; set; }
         public string LongName { get; set; }
         public string Term { get; set; }
         public string specialProperty { get; set; }
-        public string satLocation { get; set; }
-        public string division { get; set; }
-        public int? seatCapacity { get; set; }
+        public int seatCapacity { get; set; }
         public string DaysOld { get; set; }
         public string AltTitle { get; set; }
         public string status { get; set; }
         public string Quarter { get; set; }
         public string building { get; set; }
-        public string Description { get; set; }
+        public string DeptId { get; set; }
         public string withoutDate { get; set; }
         public string SubjectCode { get; set; }
-        public double? otherFee { get; set; }
+        public double otherFee { get; set; }
         public string regStartDate { get; set; }
         public List<DayModel> dayModels { get; set; }
         public object SectionNo { get; set; }
-        public int? sortOrderOne { get; set; }
-        public int? CreditHours { get; set; }
+        public int CreditHours { get; set; }
         public List<Book> booklist { get; set; }
-        public string divid { get; set; }
-        public int? CourseNo { get; set; }
+        public string CourseNo { get; set; }
         public string withDate { get; set; }
         public string endDate { get; set; }
-        public string CourseCode { get; set; }
-        public object SectionLoc { get; set; }
+        public int SectionLoc { get; set; }
         public List<object> additionalSched { get; set; }
-        public int? openSeats { get; set; }
+        public int openSeats { get; set; }
         public string StartTime { get; set; }
         public string Faculty { get; set; }
-        public int? CourseKey { get; set; }
+        public int CourseKey { get; set; }
         public string labFee { get; set; }
         public string printedComments { get; set; }
-        public int? CourseNumber { get; set; }
+        public object CourseNumber { get; set; }
         public string waitListAllowed { get; set; }
-    }
-
-    public class CourseCatalog
-    {
-        public int? results { get; set; }
-        public bool success { get; set; }
-        public List<Row> rows { get; set; }
     }
 }
